3016d2364bf8710fbd84be0d45f72f11edf7397a
Farhan Gazi <farhan.gazi@opinioni.net>
2021-10-20 20:37:12 +0200

    NEW: implemented ai logic

--- app/models/game.ts
+++ app/models/game.ts
@@ -1,3 +1,4 @@
+import { join } from "path";
 import { mongoose } from "../../config/db";
 import { User } from "./user";
 
@@ -9,8 +10,10 @@ export interface Game extends Document {
   type: string, // simple, ai
   player1: User,
   player2: User,
-  winner: User,
-  turn: User,
+  winner: any,
+  active: any,
+  dropAllowed: any,
+  aiComments: string[],
   make_move: (player_id: string, row: number, col: number) => boolean,
   terminate: () => boolean,
   add_player: (player: User) => boolean
@@ -37,15 +40,21 @@ const game_schema = new Schema({
     type: mongoose.SchemaTypes.ObjectId,
     ref: 'User'
   },
-  turn: {
-    type: mongoose.SchemaTypes.String
+  active: {
+    type: mongoose.SchemaTypes.Any
+  },
+  dropAllowed: {
+    type: mongoose.SchemaTypes.Any
+  },
+  aiComments: {
+    type: [mongoose.SchemaTypes.String]
   }
 });
 
 game_schema.methods = {
-  make_move: function (player: string, row: number, col: number): boolean {
+  make_move: function (player_id: string, row: number, col: number): boolean {
     console.log(this.turn);
-    console.log(player);
+    console.log(player_id);
     return false;
   },
 
@@ -141,6 +150,130 @@ game_schema.methods = {
     });
   },
 
+  __ai: function () {
+    var that = this;
+    var decision = null;
+    var responseType = null;
+    function threatDetect(lt, type) {
+      //vertical threat assessment & response
+      var columns = that.__getColumns();
+      for (var i = 0; i < columns.length; i++) {
+        var vertMatch;
+        type == 'major' ? vertMatch = "0" + lt + lt + lt : vertMatch = "00" + lt + lt;
+        var colMatch = columns[i].join('').match(vertMatch);
+        if (colMatch) {
+          decision = i;
+          console.log('ai: responding to a ' + type + ' vertical ' + responseType);
+          that.aiComments.push('ai: responding to a ' + type + ' vertical ' + responseType);
+        }
+      }
+
+      if (!decision) {
+        //horiz threat assessment & response
+        var horizThreatPatterns;
+        if (type == 'major') {
+          horizThreatPatterns = ['0' + lt + lt + lt, lt + '0' + lt + lt, lt + lt + '0' + lt, lt + lt + lt + '0'];
+        } else {
+          horizThreatPatterns = ['00' + lt + lt, '0' + lt + lt + '0', '0' + lt + '0' + lt, lt + '0' + lt + '0', '0' + lt + lt + '0', lt + lt + '00'];
+        }
+
+        for (i = 0; i < that.table.length; i++){
+          var found  = [];
+          var joined = that.table[i].join('');
+          for (var j = 0; j < horizThreatPatterns; j++) {
+            var match = joined.match(horizThreatPatterns[j]);
+            if (match) found.push(match[0]);
+          }
+          if (found.length) {
+            var testCase = 0;
+            if (i == that.table.length - 1) {
+              if (found[0] == '00yy' || found[0] == '00rr') testCase = 1;
+              decision = joined.indexOf(found[0]) + found[0].indexOf('0') + testCase;
+              console.log('ai: responding to a ' + type + ' horizontal ' + responseType);
+              that.aiComments.push('ai: responding to a ' + type + ' horizontal ' + responseType);
+            } else {
+              var matchPosition = joined.indexOf(found[0]) + found[0].indexOf('0');
+              if (found[0] == '00yy' || found[0] == '00rr') matchPosition++;
+              if (that.table[i + 1][matchPosition] !== 0) {
+                decision = matchPosition;
+                console.log('ai: responding to a ' + type + ' horizontal ' + responseType);
+                that.aiComments.push('ai: responding to a ' + type + ' horizontal ' + responseType);
+              }
+            }
+          }
+        }
+      }
+
+      if (!decision) {
+        //diag threat assessment & response
+        var diags = that.__getDiags();
+        var diagThreatPatterns = ['0' + lt + lt + lt, lt + '0' + lt + lt, lt + lt + '0' + lt, lt + lt + lt + '0'];
+        for(i = 0; i < diags.length; i++){
+          var found  = [];
+          var joined = diags[i].join('');
+          for(var j = 0; j < diagThreatPatterns.length; j++) {
+            var match = joined.match(diagThreatPatterns[j]);
+            if (match) found.push(match[0]);
+          }
+          if (found.length) {
+            for (var l = 0; l < found.length; l++) {
+              var diagMap = that.__getDiags([[0, 1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12, 13], [14, 15, 16, 17, 18, 19, 20], [21, 22, 23, 24, 25, 26, 27], [28, 29, 30, 31, 32, 33, 34], [35, 36, 37, 38, 39, 40, 41]]);
+              var vulnSlot = diagMap[i][found[l].indexOf('0')];
+              if (typeof that.table[Math.floor(vulnSlot / 7) + 1] === 'undefined' || that.table[Math.floor(vulnSlot / 7) + 1][(vulnSlot % 7)] !== 0) {
+                decision = vulnSlot % 7;
+                console.log('ai: responding to a ' + type + ' diagonal ' + responseType);
+                that.aiComments.push('ai: responding to a ' + type + ' diagonal ' + responseType);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    function opportunityDetect(type) {
+      //detecting our opportunities is just like detecting threats (mostly, 3 extra patterns)
+      //we want to be defensive over offensive, so we only look for opportunities
+      //if there are no immediate threats
+      responseType = 'opportunity';
+      threatDetect(that.active, type);
+    }
+
+    //look for winning opportunities
+    opportunityDetect('major');
+
+    //if none, look for major threats
+    if (decision === null) {
+      responseType = 'threat';
+      threatDetect((that.active == 'r' ? 'y' : 'r'), 'major');
+    }
+
+    //if none look for minor opportunities
+    if (decision === null) {
+      opportunityDetect('minor');
+    }
+
+    //if none look for minor threats
+    if (decision === null) {
+      responseType = 'threat';
+      threatDetect((that.active == 'r' ? 'y' : 'r'), 'minor');
+    }
+
+    if (decision !== null && that.table[0][decision] === 0) {
+      that.__drop(0, decision);
+    } else {
+      console.log('ai: no threats or opportunities found, goin random');
+      var random = Math.floor(Math.random() * 7);
+      var failSafe = 0;
+      var boardValue = that.table[0][random];
+      while (boardValue !== 0 && failSafe < 100) {
+        random = Math.floor(Math.random() * 7);
+        boardValue = that.table[0][random];
+        failSafe++;
+      }
+      that.__drop(0, random);
+    }
+  },
+
   terminate: function (): boolean {
     return false;
   },
@@ -192,7 +325,7 @@ export function new_game(data: {type: String}) {
     status: 'created',
     table: table,
     type: data.type,
-
+    active: 'r'
   });
 
   return game;
