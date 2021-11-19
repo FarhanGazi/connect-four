d1cf1045f19e479337ba283242f669f16bdd82b2
farhan.gazi <farhan.gazi@opinioni.net>
2021-11-19 13:27:19 +0100

    NEW: updated enpoints list

--- app/routers/index.ts
+++ app/routers/index.ts
@@ -3,7 +3,7 @@ import express = require('express');
 var indexRouter = express.Router();
 // GET home page.
 indexRouter.get('/', (req, res) => {
-  res.status(200).json({ api_version: "1.0", endpoints: ["/auth", "/users", "/games", "/chat"] });
+  res.status(200).json({ api_version: "1.0", endpoints: ["/auth", "/users", "/games", "/messages"] });
 });
 
 export { indexRouter }
